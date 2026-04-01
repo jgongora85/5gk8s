89e7e4edf1f8163b94cbd5ff60729a1b91bcc06b
jgongora85 <juliangongora@gmail.com>
2026-04-01 12:52:02 -0400

    Update 02_install_prerequisitos.sh

--- 02_install_prerequisitos.sh
+++ 02_install_prerequisitos.sh
@@ -27,7 +27,7 @@ fi
 
 # 6. Repositorio y Binarios de Kubernetes
 echo -n "Instalando Kubeadm, Kubelet y Kubectl... "
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg > /dev/null 2>&1
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg make gcc > /dev/null 2>&1
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg > /dev/null 2>&1
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
